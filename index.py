c8fb74a2982274e5c1b850a902e1188604f61789
Mykola Dvornyk <147269348+MykolaDvornyk11@users.noreply.github.com>
2024-02-04 11:44:36 -0500

    final project
    ended project

---
+++ index.py
@@ -0,0 +1,13 @@
+ggg(vars)
+all-{h1}
+htmls(from)
+{to}-12344431
+(JavaScoopt)
+vars11(ValueError)
+(while)
+<footer>This is a function functioning functionaly
+-set value 11111111-backend programing</footer>
+(10)-{from}(11values)
+(80)-Python snake
+(12){hex}({function-blue})
+(last-item class){vars dream-value set}
---
+++ si.c++
@@ -0,0 +1,1 @@
+c++{11function true}
